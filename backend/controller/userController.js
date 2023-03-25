8e5cbd7b3718db88699b230ec258f111dce5f800
PauloGerson <35962308+PauloGerson@users.noreply.github.com>
2023-03-25 03:28:41 -0300

    subindo tag

---
+++ backend/controller/userController.js
@@ -0,0 +1,11 @@
+import {db} from '../model/db.js'
+
+export const getUser = (_, res) => {
+    const q = "SELECT * FROM usuarios";
+
+    db.query(q, (err,data) =>{
+        if (err) return res.json(err);
+
+        return res.status(200).json(data)
+    })
+}
